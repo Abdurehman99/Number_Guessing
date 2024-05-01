4459d2e65f895c3ff4f812f2111e0eb3fa6780bc
Abdurehman99 <159618780+Abdurehman99@users.noreply.github.com>
2024-05-01 12:09:41 +0500

    Number guessing
    this inquirer will have its own number u have to guess it wil ltell weather ur answer is correct or not

---
+++ main.js
@@ -0,0 +1,14 @@
+#! /usr/env node
+import inquirer from "inquirer";
+let number = Math.floor((Math.random() * 4) + 1);
+let user = await inquirer.prompt({
+    type: 'number',
+    name: 'guessing',
+    message: 'name a number between 1-4'
+});
+if (user.guessing === number) {
+    console.log("Congratulation you gussed it!");
+}
+else {
+    console.log(`Sorry Try again the answer is incorrect the correct answer is: ${number}`);
+}
